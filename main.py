4b9a9159ae4538b8c243aadb0d0cbc6adc7bef36
saeio <142094070+saeio@users.noreply.github.com>
2024-03-28 20:54:23 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+nome = input("qm é tu")
+print(f"{nome}, tu quer faze oq? sair de casa ou ir dormir:")
+resp = input()
+if resp == "sair de casa" :
+    print(f"{nome} saiu de casa.")
+    q = input("agora tu quer fazer oq? ir pro parque ou pra lanchonete?")
+    if q == "ir pro parque":
+        print(f"{nome} foi pro parque e deu o cu")
+    elif q == "ir pra lanchonete" or "pra lanchonete":
+        print(f"{nome} foi pra lanconete come uns xtudo")
+        print("tu comeu quantos?")
+        quantos = input()
+        print(f"carai, {quantos} xtudokkkkkkkkkkkkkkkk")
+elif resp == "ir dormir":
+    print(f"{nome} foi dormir.")
